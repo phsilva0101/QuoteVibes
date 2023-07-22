6d8f1a5751ef687c01ed22bf8ca8e40dc8fc44cd
Paulo Henrique Silva <paulohsilva2001@gmail.com>
2023-07-22 01:23:46 -0300

    feat: creating controller thougths

---
+++ QuoteVibes.Domain/ViewModels/Pensamento/Response/PensamentoModel.cs
@@ -0,0 +1,44 @@
+﻿using QuoteVibes.Domain.Entities.Pensamentos;
+
+namespace QuoteVibes.Domain.ViewModels.Pensamento.Response
+{
+    public class PensamentoModel
+    {
+        public Guid Id { get; set; }
+        public string Conteudo { get; set; }
+        public string Autor { get; set; }
+        public string Modelo { get; set; }
+
+
+    }
+
+    public static class PensamentoModelExtensions
+    {
+        public static PensamentoModel ToMapModel(this Pensamentos entity)
+        {
+            return new PensamentoModel
+            {
+                Id = entity.Id,
+                Conteudo = entity.Conteudo,
+                Autor = entity.Autor,
+                Modelo = entity.Modelo
+            };
+        }
+
+        public static IEnumerable<PensamentoModel> ToMapModel(this IEnumerable<Pensamentos> entities)
+        {
+            return entities.Select(ToMapModel);
+        }
+
+        public static Pensamentos ToEntity(this PensamentoModel entity)
+        {
+            return new Pensamentos
+            {
+                Id = entity.Id,
+                Conteudo = entity.Conteudo,
+                Autor = entity.Autor,
+                Modelo = entity.Modelo
+            };
+        }
+    }
+}
---
+++ QuoteVibes/Controllers/PensamentosController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using QuoteVibes.Domain.Base;
+using QuoteVibes.Domain.Interface.Pensamento;
+using QuoteVibes.Domain.ViewModels.Pensamento.Response;
+
+namespace QuoteVibes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PensamentosController : ControllerBase
+    {
+
+        private readonly ILogger<PensamentosController> _logger;
+        private readonly IPensamentosRepository _pensamentosRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PensamentosController(ILogger<PensamentosController> logger,
+            IPensamentosRepository pensamentosRepository,
+            IUnitOfWork unitOfWork
+            )
+        {
+            _logger = logger;
+            _pensamentosRepository = pensamentosRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetThoughts()
+        {
+            var entidades = await _pensamentosRepository.GetAllAsync();
+            if (entidades != null && entidades.Any())
+                return Ok(entidades.ToMapModel());
+
+            return Ok(new List<PensamentoModel>());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetThought(Guid id)
+        {
+            var entidade = await _pensamentosRepository.GetAsync(id);
+            if (entidade != null)
+                return Ok(entidade.ToMapModel());
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostThought(PensamentoModel model)
+        {
+            var entidade = await _pensamentosRepository.AddAsync(model.ToEntity());
+
+            if (entidade == null)
+                return BadRequest("Ocorreu um erro ao inserir");
+
+            await _unitOfWork.CommitarTransacaoAsync();
+            return CreatedAtAction(nameof(GetThought), new { id = entidade.Id }, entidade.ToMapModel());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutThought(Guid id, PensamentoModel model)
+        {
+            var entidade = await _pensamentosRepository.GetAsync(id);
+            if (entidade == null)
+                return NotFound();
+
+            entidade = model.ToEntity();
+            _pensamentosRepository.Update(entidade);
+            await _unitOfWork.CommitarTransacaoAsync();
+            return NoContent();
+        }
+    }
+}
